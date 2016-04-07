4e1c26b4989b44531fe8d0f86c25de7577c1ea84
James Hycner <hycner@gmail.com>
2016-04-06 23:34:07 -0700

    Fix deployment port issue

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var config = require("./webpack.config");
 var app = express();
 var compiler = webpack(config);
 
+var port = process.env.PORT || 3000; // 3000 for testing
+
 app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
@@ -19,7 +21,7 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, "localhost", function (err) {
+app.listen(port, "localhost", function (err) {
   if (err) {
     console.log(err);
     return;
